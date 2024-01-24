e8ab1c09ecbd2720c35c7c3f2ec9fb0fab62d223
Artur <artgouveiag@gmail.com>
2024-01-24 15:26:06 +0100

    header, footer, login, create account and password pages

---
+++ src/app/_components/Header/MobileNav/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Header } from '../../../../payload/payload-types'
+
+export const MobileNav = ({ header}: { header: Header }) => {
+  return (
+    <div>MobileNav</div>
+  )
+}
